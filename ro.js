2f4cb8be057d0c34a6aea41bf095d1ad771f190b
Yathi <bm.thunderemperor@gmail.com>
2019-06-06 15:13:46 -0400

    :sparkles: The RO rule works for a single category value or an array of values

--- ro.js
+++ ro.js
@@ -6,9 +6,12 @@ function id(x) { return x[0]; }
 const moo = require("moo");
 
 const lexer = moo.compile({
-  WS:     /[ \t]+/,
+  ws:     /[ \t]+/,
   lparen:  '(',
   rparen:  ')',
+  rbracket: ']',
+  lbracket: '[',
+  comma: ',',
   boolean: ['true', 'false'],
   label: ['CATEGORY', 'BRAND'],
   eqOperator: '==',
@@ -19,16 +22,26 @@ const lexer = moo.compile({
 var grammar = {
     Lexer: lexer,
     ParserRules: [
-    {"name": "expression", "symbols": [(lexer.has("label") ? {type: "label"} : label), "param", "equalityOperation", "value"], "postprocess": d => d.join('')},
-    {"name": "param$ebnf$1", "symbols": []},
-    {"name": "param$ebnf$1", "symbols": ["param$ebnf$1", (lexer.has("WS") ? {type: "WS"} : WS)], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
-    {"name": "param$ebnf$2", "symbols": []},
-    {"name": "param$ebnf$2", "symbols": ["param$ebnf$2", (lexer.has("WS") ? {type: "WS"} : WS)], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
-    {"name": "param", "symbols": [(lexer.has("lparen") ? {type: "lparen"} : lparen), "param$ebnf$1", (lexer.has("boolean") ? {type: "boolean"} : boolean), "param$ebnf$2", (lexer.has("rparen") ? {type: "rparen"} : rparen)], "postprocess": d => d.join('')},
+    {"name": "expression", "symbols": [(lexer.has("label") ? {type: "label"} : label), "argument", "equalityOperation", "param"], "postprocess": d => d.join('')},
+    {"name": "argument$ebnf$1", "symbols": []},
+    {"name": "argument$ebnf$1", "symbols": ["argument$ebnf$1", (lexer.has("ws") ? {type: "ws"} : ws)], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "argument$ebnf$2", "symbols": []},
+    {"name": "argument$ebnf$2", "symbols": ["argument$ebnf$2", (lexer.has("ws") ? {type: "ws"} : ws)], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "argument", "symbols": [(lexer.has("lparen") ? {type: "lparen"} : lparen), "argument$ebnf$1", (lexer.has("boolean") ? {type: "boolean"} : boolean), "argument$ebnf$2", (lexer.has("rparen") ? {type: "rparen"} : rparen)], "postprocess": d => d.join('')},
     {"name": "equalityOperation", "symbols": [(lexer.has("eqOperator") ? {type: "eqOperator"} : eqOperator)]},
     {"name": "equalityOperation", "symbols": [(lexer.has("neOperator") ? {type: "neOperator"} : neOperator)]},
-    {"name": "value", "symbols": [(lexer.has("string") ? {type: "string"} : string)]},
-    {"name": "value", "symbols": [(lexer.has("number") ? {type: "number"} : number)]}
+    {"name": "param", "symbols": ["values"]},
+    {"name": "param", "symbols": ["singleValue"]},
+    {"name": "values$ebnf$1", "symbols": ["singleValueWithComma"]},
+    {"name": "values$ebnf$1", "symbols": ["values$ebnf$1", "singleValueWithComma"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "values", "symbols": [(lexer.has("lbracket") ? {type: "lbracket"} : lbracket), "values$ebnf$1", (lexer.has("rbracket") ? {type: "rbracket"} : rbracket)], "postprocess": d => d.join('')},
+    {"name": "singleValueWithComma$ebnf$1", "symbols": [(lexer.has("comma") ? {type: "comma"} : comma)], "postprocess": id},
+    {"name": "singleValueWithComma$ebnf$1", "symbols": [], "postprocess": function(d) {return null;}},
+    {"name": "singleValueWithComma$ebnf$2", "symbols": []},
+    {"name": "singleValueWithComma$ebnf$2", "symbols": ["singleValueWithComma$ebnf$2", (lexer.has("ws") ? {type: "ws"} : ws)], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "singleValueWithComma", "symbols": ["singleValue", "singleValueWithComma$ebnf$1", "singleValueWithComma$ebnf$2"], "postprocess": d => d.join('')},
+    {"name": "singleValue", "symbols": [(lexer.has("string") ? {type: "string"} : string)]},
+    {"name": "singleValue", "symbols": [(lexer.has("number") ? {type: "number"} : number)]}
 ]
   , ParserStart: "expression"
 }
