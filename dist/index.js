a9076840c2e8771d58092b8d1dfc3092d3ddfb6e
Yathi <bm.thunderemperor@gmail.com>
2019-06-13 16:24:22 -0400

    :wrench: Removed dist from gitignore. It needs to be committed so that the lib can be used without compiling.

---
+++ dist/index.js
@@ -0,0 +1,25 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+  typeof define === 'function' && define.amd ? define(['exports'], factory) :
+  (global = global || self, factory(global.RoParser = {}));
+}(this, function (exports) { 'use strict';
+
+  const nearley = require('nearley');
+  const grammar = require('./compiled-grammar/ro');
+
+  function parseFilterString(input) {
+    // Create a Parser object from our grammar.
+    const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
+    parser.feed(input);
+
+    const results = parser.results;
+    if (results.length !== 1) return 'Ambigous grammar. Bad!';
+
+    return results[0];
+  }
+
+  exports.parseFilterString = parseFilterString;
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+}));
