77d2876d48bb4070216335cb07ecda46c581b6db
Yathi <bm.thunderemperor@gmail.com>
2019-07-08 15:24:11 -0400

    :tada: :white_check_mark: Adding test cases

---
+++ src/add-to-filter.test.js
@@ -0,0 +1,8 @@
+import { addToFilter } from './ro-filter-parser.js';
+
+test('can add a category code to the filter definition', () => {});
+test('can remove a category code from the filter definition', () => {});
+test('can add a category code to the filter definition when it has only SIV attributes', () => {});
+test('can add a SIV ID to the filter definition when it does not currently have an SIV ID attribute', () => {});
+test('can add a SIV ID to the filter definition when it currently has an SIV ID attribute', () => {});
+test('can remove a SIV ID to the filter definition when it currently has an SIV ID attribute', () => {});
