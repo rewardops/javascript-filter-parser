d4945104606d15de7db7cf152b8088b4586afdb2
Yathi <bm.thunderemperor@gmail.com>
2019-07-23 10:47:43 -0400

    :heavy_plus_sign: :wrench: Trynig to get ramda to work

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,27 @@
+import commonjs from 'rollup-plugin-commonjs';
+
 const pkg = require('./package.json');
+
 const libraryName = 'ro-filter-parser';
 export default {
   input: `src/${libraryName}.js`,
   output: [
-    { file: pkg.main, name: 'RoFilterParser', format: 'umd', sourcemap: true },
+    {
+      file: pkg.main,
+      name: 'RoFilterParser',
+      format: 'umd',
+      sourcemap: true,
+    },
     { file: pkg.module, format: 'es', sourcemap: true },
+  ],
+  plugins: [
+    commonjs({
+      namedExports: {
+        // left-hand side can be an absolute path, a path
+        // relative to the current directory, or the name
+        // of a module in node_modules
+        'node_modules/ramda/index.js': ['ramda'],
+      },
+    }),
   ],
 };
