ca2ed42fc5776aa6f6fa22f06e7d5599fb2a7dd9
Kristyna Svecova <kristyna@rewardops.com>
2021-01-28 06:21:16 -0800

    Improve tests

--- test/object-to-string.test.js
+++ test/object-to-string.test.js
@@ -1,11 +1,12 @@
+import faker from 'faker';
 import convertObjectToString from '../lib/util/object-to-string';
 
 const cat1 = 'Pikachu_1';
 const cat2 = 'Raichu_2';
 const siv1 = 123;
 const siv2 = 214;
-const randomString = 'random string';
-const randomNumber = 42;
+const randomString = faker.lorem.words();
+const randomNumber = faker.random.number();
 
 describe('Valid inputs', () => {
   test('SIV included returns correct filter string', () => {
@@ -77,44 +78,17 @@ describe('Valid inputs', () => {
 });
 
 describe('Invalid inputs', () => {
-  test('0 returns empty string', () => {
-    const inputObject = 0;
-    const expectedOutput = '';
-    expect(convertObjectToString(inputObject)).toStrictEqual(expectedOutput);
-  });
-  test('a number returns empty string', () => {
-    const inputObject = randomNumber;
-    const expectedOutput = '';
-    expect(convertObjectToString(inputObject)).toStrictEqual(expectedOutput);
-  });
-  test('empty string returns empty string', () => {
-    const inputObject = '';
-    const expectedOutput = '';
-    expect(convertObjectToString(inputObject)).toStrictEqual(expectedOutput);
-  });
-  test('a string returns empty string', () => {
-    const inputObject = randomString;
-    const expectedOutput = '';
-    expect(convertObjectToString(inputObject)).toStrictEqual(expectedOutput);
-  });
-  test('empty array returns empty string', () => {
-    const inputObject = [];
-    const expectedOutput = '';
-    expect(convertObjectToString(inputObject)).toStrictEqual(expectedOutput);
-  });
-  test('empty object returns empty string', () => {
-    const inputObject = {};
-    const expectedOutput = '';
-    expect(convertObjectToString(inputObject)).toStrictEqual(expectedOutput);
-  });
-  test('undefined returns null', () => {
-    const inputObject = undefined;
-    const expectedOutput = null;
-    expect(convertObjectToString(inputObject)).toStrictEqual(expectedOutput);
-  });
-  test('null returns null', () => {
-    const inputObject = null;
-    const expectedOutput = null;
-    expect(convertObjectToString(inputObject)).toStrictEqual(expectedOutput);
+  test.each`
+    input           | expected
+    ${0}            | ${''}
+    ${randomNumber} | ${''}
+    ${''}           | ${''}
+    ${randomString} | ${''}
+    ${[]}           | ${''}
+    ${{}}           | ${''}
+    ${undefined}    | ${null}
+    ${null}         | ${null}
+  `('$input returns $expected', ({ input, expected }) => {
+    expect(convertObjectToString(input)).toEqual(expected);
   });
 });
